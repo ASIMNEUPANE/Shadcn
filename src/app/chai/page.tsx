f159ca4684a056be662e9abaa240d1d6bccef02e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-28 15:30:22 +0545

    initalize next js and write some t css

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,12 @@
+
+function chaiPage() {
+  return (
+    <main className="h-full flex justify-center items-center flex-col">
+
+        <div>chaiPage</div>
+        <button className="px-4 py-2 bg-blue-500 rounded-sm m-3 hover:bg-blue-700">Test button</button>
+    </main>
+  )
+}
+
+export default chaiPage
